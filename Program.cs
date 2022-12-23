6a6d19cb12a211f942c53081c3dcb3c9e142c70e
kenanxasiyevv <kananakh@code.edu.az>
2022-12-23 15:28:25 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Ders_Ev_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i <= n; i+=6)
+                
+            
+                {
+                    Console.WriteLine(i);
+                }
+
+            
+          
+
+        }
+    }
+}
